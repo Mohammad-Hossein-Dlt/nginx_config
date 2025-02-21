2cdfb5b74b1377da801ce04f257747e89363bd36
Mohammad_Hosein.Dlt <hosein27d@gmail.com>
2025-02-21 16:12:48 +0330

    edit auto instal

--- management.sh
+++ management.sh
@@ -271,7 +271,7 @@ elif [ "$opt" = "Certificate Management" ]; then
     certificate_opt=$(select_menu "Certificate Info" "Delete Certificate")
 
     if [ "$certificate_opt" = "Certificate Info" ]; then
-        certificate_info cert_path
+        certificate_info "$cert_path"
     fi
 
 fi
