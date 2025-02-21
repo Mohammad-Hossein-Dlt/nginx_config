453aec1f307b52651a9bd019ad477645f906296b
Mohammad_Hosein.Dlt <hosein27d@gmail.com>
2025-02-22 01:10:48 +0330

    edit auto instal

--- management.sh
+++ management.sh
@@ -148,10 +148,13 @@ function opening_ports() {
 # Certificate Management
 ########################################
 
+mkdir -p "$BASE_PATH"
+CERT_BASE_PATH="/etc/ssl/files"
+
 function certificates() {
     # Directories to search for certificates
 #    directories=( "/etc/ssl/certs" "/etc/ssl/private" "/etc/pki/tls/certs" "/etc/pki/tls/private" "/etc/letsencrypt/live" )
-    directories=( "/etc/ssl/files" )
+    directories=( CERT_BASE_PATH )
     certificate_files=()
 
     # Find certificate files with common extensions (.crt, .pem, .cer)
@@ -174,7 +177,13 @@ function certificates() {
     for cert in "${certificate_files[@]}"; do
         cert_file=$(basename "$cert")
 
-        local base_name="${cert_file%.*}"
+        base_name="${cert_file%.*}"
+
+        key_path="$CERT_BASE_PATH/${base_name}.key"
+        key_file="$base_name.key"
+        if [ ! -e "$key_path" ] && [ ! -f "$key_path" ]; then
+            key_file="N/A"
+        fi
 
         # Extract domain from the certificate subject (CN)
         domains=$(openssl x509 -in "$cert" -noout -ext subjectAltName 2>/dev/null | grep -o 'DNS:[^,]*' | sed 's/DNS://g' | paste -sd ", " -)
@@ -183,11 +192,6 @@ function certificates() {
             domains="N/A"
         fi
 
-        key_path="/etc/ssl/files/${base_name}.key"
-        key_file="$base_name.key"
-        if [ ! -e "$key_path" ] && [ ! -f "$key_path" ]; then
-            key_file="N/A"
-        fi
         items["$cert"]="Cert: $cert_file | Key: $key_file | Domains: $domains"
     done
 
@@ -200,7 +204,21 @@ function certificate_info() {
     local cert_path=$1
 
     cert_file=$(basename "$cert_path")
+
+    base_name="${cert_file%.*}"
+
+    key_path="$CERT_BASE_PATH/${base_name}.key"
+    key_file="$base_name.key"
+
+    if [ ! -e "$key_path" ] && [ ! -f "$key_path" ]; then
+        key_file="N/A"
+    fi
+
     domains=$(openssl x509 -in "$cert_path" -noout -ext subjectAltName 2>/dev/null | grep -o 'DNS:[^,]*' | sed 's/DNS://g' | paste -sd ", " -)
+
+    if [ -z "$domains" ]; then
+        domains="N/A"
+    fi
 
     # Extract certificate validity dates using openssl
     dates=$(openssl x509 -in "$cert_path" -noout -dates 2>/dev/null)
@@ -220,6 +238,33 @@ function certificate_info() {
     colored_text "36" "$info"
 }
 
+
+function get_cert() {
+    local name=$1
+    TMP_CERT=$(mktemp)
+    colored_text "36" "Please enter your certificate content in nano. Save and exit when done." >&2
+    nano "$TMP_CERT" < /dev/tty > /dev/tty
+    CERTIFICATE_CONTENT=$(cat "$TMP_CERT")
+    rm -f "$TMP_CERT"
+
+    CERT_PATH="$CERT_BASE_PATH/$name.crt"
+    echo "$CERTIFICATE_CONTENT" > "$CERT_PATH"
+    echo "$CERT_PATH"
+}
+
+function get_key() {
+    local name=$1
+    TMP_KEY=$(mktemp)
+    colored_text "36" "Please enter your private key content in nano. Save and exit when done." >&2
+    nano "$TMP_KEY" < /dev/tty > /dev/tty
+    PRIVATE_KEY_CONTENT=$(cat "$TMP_KEY")
+    rm -f "$TMP_KEY"
+
+    KEY_PATH="$CERT_BASE_PATH/$name.key"
+    echo "$PRIVATE_KEY_CONTENT" > "$KEY_PATH"
+    echo "$KEY_PATH"
+}
+
 function delete_certificate() {
     local file_path=$1
     cert_file=$(basename "$file_path")
@@ -227,14 +272,14 @@ function delete_certificate() {
     local base_name="${cert_file%.*}"
 
     colored_text "32" "Removing ssl certificate '$base_name'"
-    rm -rf "/etc/ssl/files/${base_name}.crt"
-    rm -rf "/etc/ssl/files/${base_name}.key"
+    rm -rf "$CERT_BASE_PATH/${base_name}.crt"
+    rm -rf "$CERT_BASE_PATH/${base_name}.key"
 }
 
 function delete_all_certificate() {
     colored_text "32" "Removing all ssl certificates..."
-    rm -rf /etc/ssl/files/*.crt
-    rm -rf /etc/ssl/files/*.key
+    rm -rf $CERT_BASE_PATH/*.crt
+    rm -rf $CERT_BASE_PATH/*.key
 }
 
 ########################################
@@ -272,11 +317,11 @@ if [ "$opt" = "Install Requirements" ]; then
     install_requirements
 
 elif [ "$opt" = "Nginx Management" ]; then
-    nginx_opt=$(select_menu "Install Nginx" "Delete Nginx" "Add Config" "Manage Configs")
+    opt=$(select_menu "Install Nginx" "Delete Nginx" "Add Config" "Manage Configs")
 
-    if [ "$nginx_opt" = "Install Nginx" ]; then
+    if [ "$opt" = "Install Nginx" ]; then
         install_nginx
-    elif [ "$nginx_opt" = "Delete Nginx" ];then
+    elif [ "$opt" = "Delete Nginx" ];then
         colored_text "94" "Do you really want to uninstall nginx? yes or y to confirm no or n to cancel."
         read confirm
         if [[ "${confirm,,}" = "yes" || "${confirm,,}" = "y" ]];then
@@ -284,7 +329,7 @@ elif [ "$opt" = "Nginx Management" ]; then
         else
             colored_text "93" "Uninstall nginx canceled."
         fi
-    elif [ "$nginx_opt" = "Add Config" ];then
+    elif [ "$opt" = "Add Config" ];then
         if [ ! -x "$(command -v nginx)" ]; then
             colored_text "31" "Nginx not installed."
         elif [ ! -x "$(command -v ufw)" ]; then
@@ -292,12 +337,12 @@ elif [ "$opt" = "Nginx Management" ]; then
         else
             bash <(curl -Ls https://raw.githubusercontent.com/Mohammad-Hossein-Dlt/nginx_config/master/install.sh)
         fi
-    elif [ "$nginx_opt" = "Manage Configs" ];then
+    elif [ "$opt" = "Manage Configs" ];then
         colored_text "36" "test"
         files=$(configs)
         selected_file=$(select_menu "${files[@]}")
-        config_opt=$(select_menu "Delete Config" "Edit Config")
-        if [ "$config_opt" = "Delete Config" ]; then
+        opt=$(select_menu "Delete Config" "Edit Config")
+        if [ "$opt" = "Delete Config" ]; then
             colored_text "94" "Do you really want to delete config $selected_file? yes or y to confirm no or n to cancel."
             read confirm
             if [[ "${confirm,,}" = "yes" || "${confirm,,}" = "y" ]];then
@@ -305,19 +350,19 @@ elif [ "$opt" = "Nginx Management" ]; then
             else
                 colored_text "93" "Delete config $selected_file canceled."
             fi
-        elif [ "$config_opt" = "Edit Config" ]; then
+        elif [ "$opt" = "Edit Config" ]; then
             edit_config "$selected_file"
         fi
     fi
 
 elif [ "$opt" = "Firewall Management" ]; then
-    firewall_opt=$(select_menu "Firewall Status" "Install Firewall" "Delete Firewall" "Open port(s)")
+    opt=$(select_menu "Firewall Status" "Install Firewall" "Delete Firewall" "Open port(s)")
 
-    if [ "$firewall_opt" = "Firewall Status" ]; then
+    if [ "$opt" = "Firewall Status" ]; then
         firewall_status
-    elif [ "$firewall_opt" = "Install Firewall" ]; then
+    elif [ "$opt" = "Install Firewall" ]; then
         install_firewall
-    elif [ "$firewall_opt" = "Delete Firewall" ];then
+    elif [ "$opt" = "Delete Firewall" ];then
         colored_text "94" "Do you really want to uninstall firewall (ufw)? yes or y to confirm no or n to cancel."
         read confirm
         if [[ "${confirm,,}" = "yes" || "${confirm,,}" = "y" ]];then
@@ -325,31 +370,53 @@ elif [ "$opt" = "Firewall Management" ]; then
         else
             colored_text "93" "Uninstall firewall (ufw) canceled."
         fi
-    elif [ "$firewall_opt" = "Open port(s)" ];then
+    elif [ "$opt" = "Open port(s)" ];then
         opening_ports
     fi
 
 elif [ "$opt" = "Certificate Management" ]; then
 
-    declare -A names
-    certificates names
+    opt=$(select_menu "Add Certificate" "Delete All Certificates" "Manage a Certificate")
 
-    selected=$(select_menu "${names[@]}")
+    if [ "$opt" = "Add Certificate" ]; then
+        colored_text "94" "Enter certificate file name. the name must be unique"
+        read name
+        CERT_PATH=$(get_cert "$name")
+        KEY_PATH=$(get_key "$name")
 
-    cert_path=$(find_key_by_value names "$selected")
-
-    certificate_opt=$(select_menu "Certificate Info" "Delete Certificate")
-
-    if [ "$certificate_opt" = "Certificate Info" ]; then
-        certificate_info "$cert_path"
-    elif [ "$certificate_opt" = "Delete Certificate" ]; then
-        colored_text "94" "Do you really want to delete certificate $cert_path? yes or y to confirm no or n to cancel."
+        if [[ -n "$CERT_PATH" && -n "$KEY_PATH" ]]; then
+            colored_text "32" "Certificate $name created."
+        fi
+    elif [ "$opt" = "Delete All Certificates" ]; then
+        colored_text "94" "Do you really want to delete all certificates? yes or y to confirm no or n to cancel."
         read confirm
         if [[ "${confirm,,}" = "yes" || "${confirm,,}" = "y" ]];then
             delete_certificate "$cert_path"
         else
-            colored_text "93" "Delete certificate $cert_path canceled."
+            colored_text "93" "Delete all certificates canceled."
         fi
+    elif [ "$opt" = "Manage a Certificate" ]; then
+        declare -A names
+        certificates names
+
+        selected=$(select_menu "${names[@]}")
+
+        cert_path=$(find_key_by_value names "$selected")
+
+        certificate_opt=$(select_menu "Certificate Info" "Delete Certificate")
+
+        if [ "$certificate_opt" = "Certificate Info" ]; then
+            certificate_info "$cert_path"
+        elif [ "$certificate_opt" = "Delete Certificate" ]; then
+            colored_text "94" "Do you really want to delete certificate $cert_path? yes or y to confirm no or n to cancel."
+            read confirm
+            if [[ "${confirm,,}" = "yes" || "${confirm,,}" = "y" ]];then
+                delete_certificate "$cert_path"
+            else
+                colored_text "93" "Delete certificate $cert_path canceled."
+            fi
+        fi
+
     fi
 
 elif [ "$opt" = "Reinstall everything" ]; then
